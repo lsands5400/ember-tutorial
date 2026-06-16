13f935ec47d619847c6d044d8d930f0cef570615
lsands5400 <lsands@auditboard.com>
2026-06-16 10:19:46 -0400

    route params tutorial changes

--- super-rentals2/app/routes/index.js
+++ super-rentals2/app/routes/index.js
@@ -1,6 +1,25 @@
 import Route from '@ember/routing/route';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+
+initializeFaro({
+  url: 'https://faro-collector-prod-us-east-0.grafana.net/collect/6210d0df72827d03363f82869d192a7e',
+  app: {
+    name: 'super-rentals2',
+    version: '1.0.0',
+    environment: 'production'
+  },
+
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+  ],
+});
 
 export default class IndexRoute extends Route {
   async model() {
@@ -8,7 +27,7 @@ export default class IndexRoute extends Route {
     let { data } = await response.json();
 
     return data.map((model) => {
-      let { attributes } = model;
+      let { id, attributes } = model;
       let type;
 
       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
@@ -17,7 +36,7 @@ export default class IndexRoute extends Route {
         type = 'Standalone';
       }
 
-      return { type, ...attributes };
+      return { id, type, ...attributes };
     });
   }
 }
---
+++ super-rentals2/app/templates/rental.gjs
@@ -0,0 +1,5 @@
+import RentalDetailed from 'super-rentals2/components/rental/detailed';
+
+<template>
+  <RentalDetailed @rental={{@model}} />
+</template>
